76c137864407d7dba054470bb6cd3f504c34bae2
Brian Williams <heffergod@gmail.com>
2022-09-12 09:38:20 -0700

    fixed ordering of if statements to work if outlier is the last element

--- chal-outlier/outlier.js
+++ chal-outlier/outlier.js
@@ -7,22 +7,22 @@ const outlier = arr => {
     let lastOdd;
   
     for (i = 0; i < arr.length; i++){
-      if (evens >= 2 && odds > 0){
-        return lastOdd
-      }
-      if (odds >= 2 && evens > 0){
-        return lastEven
-      }
-      if (arr[i] % 2 === 0){
-        evens++
-        lastEven = arr[i]
-      }
-      if (arr[i] % 2 === 1){
-        odds++
-        lastOdd = arr[i]
-      }
+        if (arr[i] % 2 === 0){
+            evens++
+            lastEven = arr[i]
+        }
+        if (arr[i] % 2 === 1){
+            odds++
+            lastOdd = arr[i]
+        }
+        if (evens >= 2 && odds > 0){
+            return lastOdd
+        }
+        if (odds >= 2 && evens > 0){
+            return lastEven
+        }     
     }
 }
   
-console.log(outlier([160, 3, 1719, 19, 11, 13, -21]))
+console.log(outlier([3, 1719, 19, 11, 13, -21, 160]))
 console.log(outlier([2, 4, 0, 100, 13, 12, 2602, 36]))
